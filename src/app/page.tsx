3e74b92386873893e9b9ce6b1363a91b341dcea0
Isidoro Allegretti <allegretti@extendi.it>
2024-11-19 17:11:29 +0100

    added pw login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import { DarkModeToggle } from "@/components/dark-mode-toggle";
 import { Chat } from "@/components/chat";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+
+
+
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    const checkAuth = () => {
+      try {
+        const isAuthenticated = localStorage.getItem("isAuthenticated");
+        if (!isAuthenticated) {
+          router.push("/login");
+        }
+      } catch (error) {
+        console.error("Errore nel controllo dell'autenticazione:", error);
+      }
+    };
+
+    checkAuth();
+  }, [router]);
   return (
     <main className="relative container flex min-h-screen flex-col">
       <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
